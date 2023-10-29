d11901c4fb85629ee53b77828cb4d990a552ccff
Anup Chatlawar <chatlawaranup@gmail.com>
2023-10-29 14:45:51 +0530

    Update script.js

--- Web D Task 2/script.js
+++ Web D Task 2/script.js
@@ -1,1 +1,22 @@
+function addTask() {
+    const taskInput = document.getElementById("task");
+    const taskText = taskInput.value.trim();
+
+    if (taskText !== "") {
+        const taskList = document.getElementById("taskList");
+        const li = document.createElement("li");
+        li.innerHTML = `
+            <span>${taskText}</span>
+            <button class="delete" onclick="removeTask(this)">Delete</button>
+        `;
+        taskList.appendChild(li);
+        taskInput.value = "";
+    }
+}
+
+function removeTask(button) {
+    const taskList = document.getElementById("taskList");
+    taskList.removeChild(button.parentElement);
+}
+
 
